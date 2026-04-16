f244de0cfab43bb806535a37b624587ad8e237f9
acer <ashoklande472@gmail.com>
2026-04-16 23:02:20 +0530

    new one code

--- program.c
+++ program.c
@@ -4,3 +4,15 @@ int main() {
     printf("Hello Ashok Bhau!");
     return 0;
 }
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello Ashok Bhau!\n");
+    printf("Welcome to Open Source\n");   // new line added
+    return 0;
+}
